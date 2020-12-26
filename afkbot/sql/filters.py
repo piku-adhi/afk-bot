11db97d8724fca210fbfca7f477abb05e15aa693
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-12-26 20:49:34 +0530

    Update filters.py

--- afkbot/sql/filters.py
+++ afkbot/sql/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from cinderella import SUPPORT_USERS, SUDO_USERS, DEV_USERS
+from afkbot import SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 class CustomFilters(object):
