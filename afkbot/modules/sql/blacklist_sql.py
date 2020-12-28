ea0cdcb5bfe40603f5360e5f66728b340eaf2d5d
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2020-12-28 09:03:34 +0530

    Update blacklist_sql.py

--- afkbot/modules/sql/blacklist_sql.py
+++ afkbot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText
 
-from cinderella.modules.sql import SESSION, BASE
+from afkbot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
