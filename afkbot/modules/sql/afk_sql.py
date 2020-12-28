3bd50f719cefa9aee5f3859c9791ff2a9857467a
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2020-12-28 08:52:01 +0530

    Update afk_sql.py

--- afkbot/modules/sql/afk_sql.py
+++ afkbot/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, Integer
 
-from cinderella.modules.sql import BASE, SESSION
+from afkbot.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
